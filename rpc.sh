fde0a34fcb5193e1707b195bf35d031d113e3c3c
On Blockchain <131893056+on-blockchain@users.noreply.github.com>
2025-05-31 15:46:50 -0700

    Update rpc.sh

--- rpc.sh
+++ rpc.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-set -e
 
-# Check if Docker is installed
-if ! command -v docker &> /dev/null;
-then
-    echo "Installing Docker"
-    curl -fsSL https://get.docker.com -o get-docker.sh
-    sh get-docker.sh
-    rm get-docker.sh
-fi
+echo -e "Updating system..."
+sudo apt update -y && sudo apt upgrade -y
 
-# Check if Docker Compose is installed
-if ! docker-compose version &> /dev/null;
-then
-    echo "Installing Docker-Compose"
-    apt install docker-compose
+echo -e "Installing docker if not installed"
+if ! command -v docker &>/dev/null; then
+    echo -e "Docker not found. Installing Docker..."
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+	# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo usermod -aG docker $USER
+    echo -e "Docker successfully installed!"
+else
+    echo -e "Docker is installed"
 fi
 
 mkdir -p data/nethermind data/lighthouse jwtsecret
